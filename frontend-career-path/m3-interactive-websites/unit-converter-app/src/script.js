88422d605fb6bc61b4bb936670e1fb27d7dff5f1
David Chikousky <dchikou@gmail.com>
2022-02-13 16:58:16 -0600

    commit

--- frontend-career-path/m3-interactive-websites/unit-converter-app/src/script.js
+++ frontend-career-path/m3-interactive-websites/unit-converter-app/src/script.js
@@ -1,19 +1,63 @@
-let unitEl = document.getElementById("unit-el")
-let lengthEl = document.getElementById("length-el")
-let volumeEl = document.getElementById("volume-el")
-let massEl = document.getElementById("mass-el")
-let numberEl = document.getElementById("number-el")
+let unitEl = document.getElementById("unit-el");
+let lengthEl = document.getElementById("length-el");
+let volumeEl = document.getElementById("volume-el");
+let massEl = document.getElementById("mass-el");
+let numberEl = document.getElementById("number-el");
+
+const numberInputEl = document.getElementById("number-el");
+numberEl.addEventListener("input", (event) => {
+  if (event.target.value.length > numberInputEl.maxLength) {
+    event.target.value = event.target.value.slice(0, numberInputEl.maxLength);
+  }
+});
+
+const submitButton = document.getElementById("submit-button");
+submitButton.addEventListener("click", () => {
+  getNumberValue();
+});
 
 function getNumberValue() {
-    let numberVal = numberEl.value
-    if (numberVal > 0) {
-        document.getElementById("unit-el").textContent = numberVal
-        doTheMath()
-    } else {}
+  let numberVal = numberEl.value;
+  if (numberVal > 0) {
+    document.getElementById("unit-el").textContent = numberVal;
+    calcuteAndSetUnitLabels();
+  }
 }
 
-function doTheMath() {
-    lengthEl.textContent = unitEl.textContent + " " + "Meters = " + (unitEl.textContent*3.281).toFixed(3) + " Feet | " + unitEl.textContent + " Feet = " + (unitEl.textContent/3.281).toFixed(3) + " Meters"
-    volumeEl.textContent = unitEl.textContent + " " + "Litres = " + (unitEl.textContent/3.785).toFixed(3) + " Gallons | " + unitEl.textContent + " Gallons = " + (unitEl.textContent*3.785).toFixed(3) + " Litres"
-    massEl.textContent = unitEl.textContent + " " + "Kilograms = " + (unitEl.textContent*2.205).toFixed(3) + " Pounds | " + unitEl.textContent + " Pounds = " + (unitEl.textContent/2.205).toFixed(3) + " Kilograms"
+function calcuteAndSetUnitLabels() {
+  const METERS_FEET_MULTIPLIER = 3.281;
+  const LITERS_GALLONS_MULTIPLIER = 3.785;
+  const KILOS_POUNDS_MULTIPLIER = 2.205;
+
+  const input = unitEl.textContent;
+
+  lengthEl.textContent =
+    input +
+    " Meters = " +
+    (input * METERS_FEET_MULTIPLIER).toFixed(3) +
+    " Feet | " +
+    input +
+    " Feet = " +
+    (input / METERS_FEET_MULTIPLIER).toFixed(3) +
+    " Meters";
+
+  volumeEl.textContent =
+    input +
+    " Litres = " +
+    (input / LITERS_GALLONS_MULTIPLIER).toFixed(3) +
+    " Gallons | " +
+    input +
+    " Gallons = " +
+    (input * LITERS_GALLONS_MULTIPLIER).toFixed(3) +
+    " Litres";
+
+  massEl.textContent =
+    input +
+    " Kilograms = " +
+    (input * KILOS_POUNDS_MULTIPLIER).toFixed(3) +
+    " Pounds | " +
+    input +
+    " Pounds = " +
+    (input / KILOS_POUNDS_MULTIPLIER).toFixed(3) +
+    " Kilograms";
 }
